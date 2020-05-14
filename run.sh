1da8dfbae90e7f43cffcd8d96c54026d07ee232a
Leah Kemp <leah.kemp@esr.cri.nz>
2020-05-14 17:09:58 +1200

    Add branch with config/run settings for exome project on production

--- run.sh
+++ run.sh
@@ -3,4 +3,13 @@
 snakemake \
 -j 24 \
 --use-conda \
---configfile config.yaml
+--configfile ./config/GRCh38_single_WES_config_template.yaml \
+ --cluster-config cluster.json \
+ --cluster "sbatch \
+ -A {cluster.account} \
+ t {cluster.time} \
+ -n {cluster.n} \
+ -p {cluster.partition} \
+ --cpus-per-task {cluster.cpus} \
+ --mail-user {email} \
+ --mail-type {cluster.emailtype}"
